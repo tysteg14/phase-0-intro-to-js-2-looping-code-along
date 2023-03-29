153ff2de276ef91ce712abb1c72a58d1dac4c5b7
Tyrell <tysteg14@gmail.com>
2023-03-29 13:15:57 -0500

    creates

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+
+
+function writeCards(names, message) {
+    let messageArray = []
+    for (let i = 0; i < names.length; i++) {
+       messageArray.push(`Thank you, ${names[i]}, for the wonderful ${message} gift!`)
+    }
+
+    return messageArray;
+    
+}
+
+console.log(writeCards(['Guadalupe', 'Ollie', 'Aki'], "surprise"));
+
+
+
+function countDown(num) {
+  let i = num;
+  while (i >= 0 ) {
+    console.log(i);
+    i--;
+  }
+  return countDown;
+}
+
+countDown(10);
